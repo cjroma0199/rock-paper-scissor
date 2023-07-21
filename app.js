282feb9a6346563054133dc9b63720e42fb04f3f
cjrdelosreyes <122694227+cjroma0199@users.noreply.github.com>
2023-07-21 10:09:17 +0800

    finish game result modal and play again funtion

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const roundWeapons = document.querySelector(".outcome_text");
 const playerWeaponImage = document.createElement("img");
 const computerWeaponImage = document.createElement("img");
 const isBattleWindowActive = Boolean(document.querySelector(".choices"));
+const span = document.querySelector(".close");
+const modal = document.querySelector("#myModal");
+const modalContent = document.querySelector(".game_winner");
+const playAgainButton = document.querySelector("#play_again");
 const MAX_HEALTH = 100;
 
 let rounds = 0;
@@ -70,10 +74,10 @@ const updateDisplay = (result) => {
     )} beats ${capitalizedFirstLetter(result.computerWeapon)}.`;
     return;
   } else {
-    roundResult.textContent = `Computer won this round!`;
+    roundResult.textContent = `You lost this round!`;
     roundWeapons.textContent = `${capitalizedFirstLetter(
-      result.computerWeapon
-    )} beats ${capitalizedFirstLetter(result.playerWeapon)}.`;
+      result.playerWeapon
+    )} was beaten by ${capitalizedFirstLetter(result.computerWeapon)}.`;
   }
 };
 
@@ -99,51 +103,106 @@ const updateBattleWindow = (result) => {
     playerWindow.appendChild(playerWeaponImage);
     computerWindow.appendChild(computerWeaponImage);
   }
+  computerWeaponImage.style.display = "block";
+  playerWeaponImage.style.display = "block";
 };
 
-const updateHealth = (loser, loserHealth = 0) => {
-  const healthBar = document.querySelector(`.${loser.toLowerCase()}_health`);
+const getHeathBar = (player) => {
+  return document.querySelector(`.${player.toLowerCase()}_health`);
+};
 
-  healthBar.style.width = `${MAX_HEALTH - loserHealth}%`;
+const updateHealthBar = (loser, loserHealth = 0) => {
+  getHeathBar(loser).style.width = `${MAX_HEALTH - loserHealth}%`;
 };
 
 const getGameWinner = (score) => {
   return Object.keys(score).find((key) => score[key] == 5);
 };
 
-const isGameOver = (score) => {
-  return score.player == 5 || score.computer == 5;
+const isGameOver = () => {
+  return Object.values(score).includes(5);
+};
+
+const resetHealthBar = () => {
+  getHeathBar("computer").style.width = `${MAX_HEALTH}%`;
+  getHeathBar("player").style.width = `${MAX_HEALTH}%`;
+};
+
+const resetGame = () => {
+  resetHealthBar();
+
+  computerWeaponImage.style.display = "none";
+  playerWeaponImage.style.display = "none";
+  updateRoundCount((rounds = 0));
+  imageHolder.forEach((image) => {
+    image.style.display = "block";
+    image.classList.remove("hide");
+  });
+
+  score.computer = 0;
+  score.player = 0;
+
+  roundResult.textContent = "Let's beat the computer!";
+  roundWeapons.textContent = "Good luck!";
 };
 
 weapons.forEach((weapon) =>
   weapon.addEventListener("click", (event) => {
-    if (isGameOver(score)) {
-      const gameWinner = getGameWinner(score);
-      console.log(gameWinner);
+    if (isGameOver()) {
+      modal.style.display = "block";
+      return;
     }
+    if (!isGameOver()) {
+      let result = getWinner(getPlayerWeapon(weapon), getComputerWeapon());
+      let loserHealth = 0;
 
-    let result = getWinner(getPlayerWeapon(weapon), getComputerWeapon());
-    let loserHealth = 0;
-
-    if (result.winner == "Player") {
-      ++score.player;
-      loserHealth = score.player * 20;
-    } else if (result.winner == "Computer") {
-      ++score.computer;
-      loserHealth = score.computer * 20;
-    }
-
-    hideImageHolder();
-
-    setTimeout(() => {
-      updateBattleWindow(result);
-      updateDisplay(result);
-      updateRoundCount(++rounds);
-
-      if (!result.loser == "") {
-        updateHealth(result.loser, loserHealth);
+      if (result.winner == "Player") {
+        ++score.player;
+        loserHealth = score.player * 20;
+      } else if (result.winner == "Computer") {
+        ++score.computer;
+        loserHealth = score.computer * 20;
       }
-    }, 300);
-    console.log(loserHealth);
+      hideImageHolder();
+
+      setTimeout(() => {
+        updateBattleWindow(result);
+        updateDisplay(result);
+        updateRoundCount(++rounds);
+
+        if (!result.loser == "") {
+          updateHealthBar(result.loser, loserHealth);
+        }
+      }, 300);
+
+      if (isGameOver()) {
+        setTimeout(() => {
+          const gameWinner = getGameWinner(score);
+
+          if (gameWinner == "player") {
+            modalContent.textContent = "You won!";
+          } else {
+            modalContent.textContent = "You lost..";
+          }
+          modal.style.display = "block";
+        }, 800);
+        return;
+      }
+    }
   })
 );
+
+span.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+playAgainButton.onclick = () => {
+  resetGame();
+  modal.style.display = "none";
+};
